4df327b338260550e01536d25cc3279ecc835b12
Andrew Smith <andrewmsmith66@gmail.com>
2023-05-15 14:44:54 -0400

    Adds ability to create a new tweet.

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,17 +1,64 @@
 import { ProfileImage } from "~/components/ProfileImage";
 import { Button } from "~/components/Button";
 import { useSession } from "next-auth/react";
+import {
+  type FormEvent,
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import { api } from "~/utils/api";
 
-export function NewTweetForm() {
+function updateTextAreaSize(textarea?: HTMLTextAreaElement) {
+  if (textarea == null) return;
+  textarea.style.height = "0px";
+  textarea.style.height = `${textarea.scrollHeight}px`;
+}
+
+export function Form() {
   const session = useSession();
+
+  const [inputValue, setInputValue] = useState("");
+
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
+
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: () => {
+      setInputValue("");
+    },
+  });
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (inputValue.trim().length === 0) return;
+    createTweet.mutate({ content: inputValue });
+  }
 
   if (session.status !== "authenticated") return null;
 
   return (
-    <form action="" className="flex flex-col gap-2 border-b px-4 py-2">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 border-b px-4 py-2"
+    >
       <div className="flex gap-4">
         <ProfileImage src={session.data?.user.image} />
         <textarea
+          ref={inputRef}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          style={{ height: 0 }}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="What's happening?"
         />
@@ -19,4 +66,10 @@ export function NewTweetForm() {
       <Button className="self-end">Tweet</Button>
     </form>
   );
+}
+
+export function NewTweetForm() {
+  const session = useSession();
+  if (session.status !== "authenticated") return null;
+  return <Form />;
 }
