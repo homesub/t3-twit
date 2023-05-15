0bba6868ae6f407d539823f08331f5b0999fd5e1
Andrew Smith <andrewmsmith66@gmail.com>
2023-05-15 17:34:58 -0400

    Adds get tweets with infinite scroll.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { type NextPage } from "next";
 
 import { NewTweetForm } from "~/components/NewTweetForm";
+import { InfiniteTweetList } from "~/components/InfiniteTweetList";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   return (
@@ -9,8 +11,28 @@ const Home: NextPage = () => {
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
       </header>
       <NewTweetForm />
+      <RecentTweets />
     </>
   );
 };
+
+function RecentTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    {},
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  if (!tweets || !tweets.hasNextPage) return null;
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
+}
 
 export default Home;
