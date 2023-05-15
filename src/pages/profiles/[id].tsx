cbd5fd97f8109a37c49988ec43018d4706559dd9
Andrew Smith <andrewmsmith66@gmail.com>
2023-05-15 14:49:33 -0400

    Creates mock profile page to stop build errors.

---
+++ src/pages/profiles/[id].tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next";
+
+const ProfilePage: NextPage = () => {
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="mb-2 px-4 text-lg font-bold">Profile Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default ProfilePage;
